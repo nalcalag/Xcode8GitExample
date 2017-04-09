876c8f513d2cf82a96d1cce98ee857eb4316ab42
Noelia <nalcalag@uoc.edu>
2017-04-09 16:05:59 +0100

    Initial Commit

---
+++ Xcode8GitExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8GitExample
+//
+//  Created by Noelia Alcala on 09/04/2017.
+//  Copyright © 2017 Noelia Alcala. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Noelia Alcala on 09/04/2017.
+//  Copyright © 2017 Noelia Alcala. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
